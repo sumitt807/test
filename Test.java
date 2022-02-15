bfe3ad07ab214a89a10656293626c03acd9ff8a1
Sumit <sumit.webnware@gmail.com>
2022-02-15 19:07:44 +0530

    dsd

---
+++ Test.java
@@ -0,0 +1,35 @@
+import java.awt.Robot;
+import java.awt.event.InputEvent;
+import java.util.Random;
+import java.awt.MouseInfo;
+import java.util.concurrent.TimeUnit;
+import java.awt.Point;
+
+public class Test {
+    public static final int FIVE_SECONDS = 30000;
+    public static final int MAX_Y = 400;
+    public static final int MAX_X = 400;
+
+    public static void main(String... args) throws Exception {
+        Robot robot = new Robot();
+        Random random = new Random();
+        int flag = 0;
+        while (true) {
+            Point p = MouseInfo.getPointerInfo().getLocation();
+            int mouseX = p.x;
+            if (mouseX > 1 && flag == 0) {
+                mouseX -= 1;
+                flag = 1;
+            } else {
+                mouseX += 1;
+                flag = 0;
+            }
+            // robot.mouseMove(mouseX, mouseX);
+
+            robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+            robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+
+            Thread.sleep(FIVE_SECONDS);
+        }
+    }
+}
